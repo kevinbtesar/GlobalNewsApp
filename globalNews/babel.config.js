54c7fab77abce9b22f0db9c5d3f6b9162c89668a
kevinbtesar <54149309+kevinbtesar@users.noreply.github.com>
2024-06-08 09:47:56 -0500

    Initial commit

--- globalNews/babel.config.js
+++ globalNews/babel.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    'react-native-reanimated/plugin',
-  ],
-  env: {
-    production: {
-      plugins: ['react-native-paper/babel'],
-    },
-  },
-
+  presets: ['module:@react-native/babel-preset'],
 };
